c0a087b0fb40b51f7aef26bcc3ce54508c7ab137
Misho Skhirtladze <mishka.sxirtladze@gmail.com>
2026-07-21 00:02:44 +0400

    test

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -55,7 +55,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 
 const currentWeather = async (locationKey) => {
   const res = await fetch(
-    `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?details=true&getPhotos=true`,
+    `/api/weather/current/${encodeURIComponent(locationKey)}?details=true&getPhotos=true`,
     {
       headers: {
         authorization: apiKey,
