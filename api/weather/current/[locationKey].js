f1a01066020abd5a12608227fec555660d8cd633
Misho Skhirtladze <mishka.sxirtladze@gmail.com>
2026-07-27 23:57:25 +0400

    test

--- api/weather/current/[locationKey].js
+++ api/weather/current/[locationKey].js
@@ -1,39 +1,39 @@
-// export default async function handler(req, res) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-//   if (req.method === "OPTIONS") {
-//     return res.status(200).end();
-//   }
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-//   const { locationKey } = req.query;
-//   if (!locationKey) {
-//     return res.status(400).json({ error: 'Query parameter "q" is required' });
-//   }
+  const { locationKey } = req.query;
+  if (!locationKey) {
+    return res.status(400).json({ error: 'Query parameter "q" is required' });
+  }
 
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     return response
-//       .status(401)
-//       .json({ error: "Authorization header is required" });
-//   }
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return response
+      .status(401)
+      .json({ error: "Authorization header is required" });
+  }
 
-//   // const currentWeatherUrl = `https://dataservice.accuweather.com/currentconditions/v1/${encodeURIComponent(locationKey)}?details=true&getPhotos=true`;
-//   // try {
-//   //   const apiResponse = await fetch(currentWeatherUrl, {
-//   //     method: "GET",
-//   //     headers: {
-//   //       Authorization: authHeader,
-//   //       Accept: "application/json",
-//   //     },
-//   //   });
-//   //   const data = await apiResponse.json();
+  // const currentWeatherUrl = `https://dataservice.accuweather.com/currentconditions/v1/${encodeURIComponent(locationKey)}?details=true&getPhotos=true`;
+  // try {
+  //   const apiResponse = await fetch(currentWeatherUrl, {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: authHeader,
+  //       Accept: "application/json",
+  //     },
+  //   });
+  //   const data = await apiResponse.json();
 
-//   //   return res.status(apiResponse.status).json(data);
-//   // } catch (error) {
-//   //   return res
-//   //     .status(500)
-//   //     .json({ error: "Internal server error fetching weather data" });
-//   // }
-// }
+  //   return res.status(apiResponse.status).json(data);
+  // } catch (error) {
+  //   return res
+  //     .status(500)
+  //     .json({ error: "Internal server error fetching weather data" });
+  // }
+}
