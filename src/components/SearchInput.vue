16de2fd2b0fa321dd536ff72ca590de6c2c74af0
Misho Skhirtladze <mishka.sxirtladze@gmail.com>
2026-08-03 21:45:59 +0400

    test

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -46,7 +46,7 @@ const currentWeather = async (locationKey) => {
       throw new Error(`Server returned code ${res.status}`);
     }
 
-    const data = await res.json();
+    const data = await res.text();
     // console.log(data);
 
     return data;
