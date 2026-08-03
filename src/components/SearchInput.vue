1fc4abbd6832d5266bdf6f74166a2f510515e288
Misho Skhirtladze <mishka.sxirtladze@gmail.com>
2026-08-03 22:19:12 +0400

    test

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -58,7 +58,7 @@ const currentWeather = async (locationKey) => {
 const getForecast = async (locationKey) => {
   try {
     const res = await fetch(
-      `/api/weather/forecast/[locationKey]/${encodeURIComponent(locationKey)}?&metric=true`,
+      `/api/weather/forecast/[locationKey]${encodeURIComponent(locationKey)}?&metric=true`,
       {
         method: "GET",
         headers: {
